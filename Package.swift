91c127b3ed0b752161d8542ab605c1f013e5dd76
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-28 12:16:34 +1000

    Depend on minor glib version (2.46)

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "GIO",
     dependencies: [
-        .Package(url: "https://github.com/rhx/SwiftGLib.git", majorVersion: 2)
+        .Package(url: "https://github.com/rhx/SwiftGLib.git", majorVersion: 2, minor: 46)
     ]
 )
