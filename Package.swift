469a33c622078cec5e9ec24e841b99433234842b
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-29 05:35:32 +1000

    Remove minor version from generic branch

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "GIO",
     dependencies: [
-        .Package(url: "https://github.com/rhx/SwiftGObject.git", majorVersion: 2, minor: 46)
+        .Package(url: "https://github.com/rhx/SwiftGObject.git", majorVersion: 2)
     ]
 )
