8e841bec4ba10fa0dc7fd26fb02f05ee3af84b18
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-08 15:41:00 +1000

    Create aliases in Gio struct (referenced by Gtk)

--- build.sh
+++ build.sh
@@ -23,4 +23,19 @@ fi
 LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $module  gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
 gir2swift -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir "${GIR}" | sed -f ${Module}.sed > Sources/${Module}.swift
+echo  > Sources/SwiftGio.swift "import CGLib"
+echo >> Sources/SwiftGio.swift "import GLib"
+echo >> Sources/SwiftGio.swift "import GObject"
+echo >> Sources/SwiftGio.swift ""
+grep 'public protocol' Sources/${Module}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _gio_\1 = \1/' >> Sources/SwiftGio.swift
+echo >> Sources/SwiftGio.swift ""
+grep 'public class' Sources/${Module}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _gio_\1 = \1/' >> Sources/SwiftGio.swift
+echo >> Sources/SwiftGio.swift ""
+echo >> Sources/SwiftGio.swift "public struct Gio {"
+grep 'public protocol' Sources/${Module}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _gio_\1/' >> Sources/SwiftGio.swift
+echo >> Sources/SwiftGio.swift ""
+grep 'public class' Sources/${Module}.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _gio_\1/' >> Sources/SwiftGio.swift
+echo >> Sources/SwiftGio.swift ""
+grep 'public typealias' Sources/${Module}.swift | sed 's/^/    /' >> Sources/SwiftGio.swift
+echo >> Sources/SwiftGio.swift "}"
 exec swift build $CCFLAGS $LINKFLAGS "$@"
