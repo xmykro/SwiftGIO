ac3a4f47f05d692fb61e2b303966e75de42b767f
Rene Hexel <r.hexel@griffith.edu.au>
2016-07-30 08:46:25 +1000

    Remove old toolchain path

--- config.sh
+++ config.sh
@@ -7,6 +7,5 @@ Mod=Gio
 Module=${Mod}-2.0
 MODULE=`echo "${Module}" | tr '[:lower:]' '[:upper:]'`
 module=`echo "${Module}" | tr '[:upper:]' '[:lower:]'`
-export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
 LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $module  gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
