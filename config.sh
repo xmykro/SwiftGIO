7b1986a21bd41ac4367e3c480ab387dd06cc444a
Rene Hexel <r.hexel@griffith.edu.au>
2017-05-27 08:03:47 +1000

    Remove '-Wl,' from linker flags

--- config.sh
+++ config.sh
@@ -9,7 +9,7 @@ MODULE=`echo "${Module}" | tr '[:lower:]' '[:upper:]'`
 module=`echo "${Module}" | tr '[:upper:]' '[:lower:]'`
 BUILD_DIR=`pwd`/.build
 export PATH="${BUILD_DIR}/gir2swift/.build/release:${BUILD_DIR}/gir2swift/.build/debug:${PATH}"
-LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
+LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $module  gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
 TAC="tail -r"
 if which tac >/dev/null ; then
