9607cf99c97be968df3b19d775b30410270ac935
Rene Hexel <r.hexel@griffith.edu.au>
2019-03-20 07:31:24 +1000

    Ensure that all sed scripts are applied in sequence

--- gir-to-swift.sh
+++ gir-to-swift.sh
@@ -24,12 +24,13 @@ fi
 gir2swift -o Sources/${MOD} -m ${Module}.module -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir "${GIR}"
 for src in Sources/${MOD}/*-*.swift ; do
 	sed -f ${Module}.sed < ${src} > ${src}.out
+	mv ${src}.out ${src}
 	for ver in 2.60 ; do
 		if pkg-config --atleast-version=$ver glib-2.0 ; then
 			sed -f ${Module}-$ver.sed < ${src} > ${src}.out
+			mv ${src}.out ${src}
 		fi
 	done
-	mv ${src}.out ${src}
 done
 touch Sources/${MOD}/${Module}.swift
 echo  > Sources/${MOD}/Swift${Mod}.swift "import CGLib"
