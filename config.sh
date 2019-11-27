f78a324c13fb33aa5d31735242b25febaa46a905
Rene Hexel <r.hexel@griffith.edu.au>
2019-11-27 16:26:24 +1000

    Add BUILD_DIR support

--- config.sh
+++ config.sh
@@ -10,7 +10,13 @@ MOD=`echo "${Mod}" | tr '[:lower:]' '[:upper:]'`
 Module=${Mod}-$VER
 MODULE=`echo "${Module}" | tr '[:lower:]' '[:upper:]'`
 module=`echo "${Module}" | tr '[:upper:]' '[:lower:]'`
-BUILD_DIR=`pwd`/.build
+if [ -z "$BUILD_DIR" ]; then
+  if `pwd -P | grep -q Dropbox` ; then
+    export BUILD_DIR="/tmp/.build-$Module"
+  else
+    export BUILD_DIR=`pwd`/.build
+  fi
+fi
 export PATH="${BUILD_DIR}/gir2swift/.build/release:${BUILD_DIR}/gir2swift/.build/debug:${PATH}:/usr/local/opt/ruby/bin:`echo /usr/local/lib/ruby/gems/*/bin | tr ' ' '\n' | tail -n1`:${PATH}:`echo /var/lib/gems/*/gems/jazzy-*/bin/ | tr ' ' '\n' | tail -n1`:/usr/local/bin"
 export PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig:${PKG_CONFIG_PATH}
 export LINKFLAGS="`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | tr '	' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//' | tr '\n' ' ' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//' | sed -e 's/-pthread/-lpthread/g' -e 's/^/-Xlinker /' -e 's/-Wl,//g' -e 's/ -pthread/ -lpthread/g' -e 's/-Xcc *-Xlinker/-Xlinker/g' -e 's/-Xlinker *-Xcc/-Xcc/g' -e 's/-Xlinker *--export-dynamic//g' -e 's/-Xcc *-Xcc/-Xcc/g' -e 's/-Xlinker *-Xlinker/-Xlinker/g' -e 's/-Xcc *$//' -e 's/-Xlinker *-Xlinker/-Xlinker/g' -e 's/-Xlinker *$//'`"
