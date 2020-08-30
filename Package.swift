c9762a2cea1527f9832720766c4780ff6e8862fa
Rene Hexel <r.hexel@griffith.edu.au>
2020-08-31 07:28:25 +1000

    Note Swift 5.2 requirement

--- Package.swift
+++ Package.swift
@@ -1,15 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "GIO",
-    products: [
-        .library(name: "GIO", targets: ["GIO"]),
-    ],
+    products: [ .library(name: "GIO", targets: ["GIO"]) ],
     dependencies: [
-        .package(url: "ssh://promac.local/Users/rh/src/swift/rh/gtk/SwiftGObject", .branch("master"))
-        //.package(url: "https://github.com/rhx/SwiftGObject.git", .branch("master"))
+        .package(name: "GLibObject", url: "https://github.com/rhx/SwiftGObject.git", .branch("master"))
     ],
     targets: [
         .target(name: "GIO", dependencies: ["GLibObject"]),
