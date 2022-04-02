0b2d617f25377c7a1d300176fa67c306008fdf2f
Rene Hexel <r.hexel@griffith.edu.au>
2022-04-02 18:54:31 +1000

    Upgrade to Swift 5.6 and use plugin

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.6
 
 import PackageDescription
 
@@ -6,11 +6,19 @@ let package = Package(
     name: "GIO",
     products: [ .library(name: "GIO", targets: ["GIO"]) ],
     dependencies: [
-        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("development")),
-        .package(name: "GLibObject", url: "https://github.com/rhx/SwiftGObject.git", .branch("development"))
+        .package(url: "https://github.com/rhx/gir2swift.git",    branch: "development"),
+        .package(url: "https://github.com/rhx/SwiftGObject.git", branch: "development")
     ],
     targets: [
-        .target(name: "GIO", dependencies: ["GLibObject"]),
+        .target(
+            name: "GIO",
+            dependencies: [
+                .product(name: "GLibObject", package: "SwiftGObject")
+            ],
+            plugins: [
+                .plugin(name: "gir2swift-plugin", package: "gir2swift")
+            ]
+        ),
         .testTarget(name: "GIOTests", dependencies: ["GIO"]),
     ]
 )
