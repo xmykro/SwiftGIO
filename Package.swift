a8fde121fdfb3cdfadf8d1a2235f725ed2e71b44
Rene Hexel <rhx@users.noreply.github.com>
2022-04-12 06:51:35 +1000

    Suppress warnings

--- Package.swift
+++ Package.swift
@@ -16,6 +16,10 @@ let package = Package(
                 .product(name: "gir2swift", package: "gir2swift"),
                 .product(name: "GLibObject", package: "SwiftGObject")
             ],
+            swiftSettings: [
+                .unsafeFlags(["-suppress-warnings"], .when(configuration: .release)),
+                .unsafeFlags(["-suppress-warnings", "-Xfrontend", "-serialize-debugging-options"], .when(configuration: .debug)),
+            ],
             plugins: [
                 .plugin(name: "gir2swift-plugin", package: "gir2swift")
             ]
